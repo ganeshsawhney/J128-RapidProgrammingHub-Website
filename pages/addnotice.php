5f115ffdfd5513a31c6a413a7563cab1bff87724
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-08-07 00:23:27 +0530

    Addnotice

--- pages/addnotice.php
+++ pages/addnotice.php
@@ -44,24 +44,39 @@ if(isset($_POST['action']))
 			echo $message;
     }
 }
-else{
 ?>
 
-<div id="login-form">
-<form method="post">
-<input name="action" type="hidden" value="login" />
-<table align="center">
-<tr>
-<td><input type="text" name="email" placeholder="Your Email" required /></td>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-<td><input type="submit" name="btn-login" value="Sign In" /></td>
-</tr>
-</table>
-</form>
+
+<div class="container">
+  <h2>Add a Notice</h2>
+  <form action="pages/upload.php" method="post" role="form">
+    <div class="form-group">
+      <label for="text">Subject:</label>
+      <input type="text" class="form-control" id="subject" placeholder="Enter Subject">
+    </div>
+    <div class="form-group">
+      <label for="text">Type:</label>
+      <input type="text" class="form-control" id="type" placeholder="Enter Type ex: Lecture/Contest/Notification">
+    </div>
+	<div class="form-group">
+      <label for="text">Date Of Event:</label>
+      <input type="date" class="form-control" id="dateofevent" placeholder="Enter Date of Event if Required">
+    </div>
+	
+    <div class="form-group">
+		<label for="notice">Notice:</label>
+		<textarea class="form-control" rows="5" id="notice" placeholder="Details here"></textarea>
+	</div>
+    
+	<div class="form-group">
+		<label for="notice">Add All Files in a zip or rar(if any):</label>    
+		<input type="file" name="fileToUpload" id="fileToUpload">
+	</div>	
+
+    <input type="submit" class="btn btn-primary"  name="submit">Submit</input>
+  </form>
 </div>
 
-<?php
-}
-?>
+
 </body>
 </html>
